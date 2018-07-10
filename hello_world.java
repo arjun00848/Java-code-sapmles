95eccae66ad1758745cad99891c3633ac674e59d
devilbits <arjun00848@gmail.com>
2018-07-10 12:44:20 +0530

    Update hello_world.java

--- hello_world.java
+++ hello_world.java
@@ -1,6 +1,10 @@
-  
+  import java.util.*;
   public class test{
+    Scanner getInput;
     public static void main(String[] args){
     System.out.println("Hello world");
+    getInput = new Scanner(Sysytem.in);
+    String value = getInput.next();
+    System.out.println("Hello"+value);  
     }
 }
